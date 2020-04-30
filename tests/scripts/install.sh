00dfa5acc38684244b2581b3f1e7531184d1a644
Chad Roberts <croberts@redhat.com>
2020-04-30 16:55:07 -0400

    Adding bits to make our tests into a container image

---
+++ tests/scripts/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Installing kfDef from test directory"
+
+set -x
+pushd /kfdef
+kfctl build -V -f ./kfctl_openshift.yaml
+kfctl apply -V -f ./kfctl_openshift.yaml
+set +x
+if [ "$?" -ne 0 ]; then
+    echo "The installation failed"
+    exit $?
+fi
+popd
+
+echo "Pausing 180 seconds to allow services to spin-up"
+sleep 180s
