2818192f2de6e4b9d925338a2296a471508a9b9e
Chad Roberts <croberts@redhat.com>
2020-05-05 10:08:17 -0400

    Adding some debug info for ci system runs and making kubeconfig local and writable.

--- tests/scripts/installandtest.sh
+++ tests/scripts/installandtest.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 set -x
+env | sort
+cp /var/run/secrets/ci.openshift.io/multi-stage/kubeconfig /root/.kube/config
+chmod 755 /root/.kube/config
+export KUBECONFIG=/root/.kube/config
 /peak/install.sh
 /peak/operator-tests/run.sh
-set +x
 if [ "$?" -ne 0 ]; then
     echo "The tests failed"
+    exit 1
 fi
