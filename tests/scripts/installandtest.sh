4648f25f591e0843b69d956034db32fb0630ebdc
Chad Roberts <croberts@redhat.com>
2020-05-05 11:17:00 -0400

    Changes for kubeconfig (#57)

--- tests/scripts/installandtest.sh
+++ tests/scripts/installandtest.sh
@@ -2,9 +2,10 @@
 
 set -x
 env | sort
-cp /var/run/secrets/ci.openshift.io/multi-stage/kubeconfig /root/.kube/config
-chmod 755 /root/.kube/config
-export KUBECONFIG=/root/.kube/config
+mkdir -p ~/.kube
+cp /var/run/secrets/ci.openshift.io/multi-stage/kubeconfig ~/.kube/config
+chmod 755 ~/.kube/config
+export KUBECONFIG=~/.kube/config
 /peak/install.sh
 /peak/operator-tests/run.sh
 if [ "$?" -ne 0 ]; then
