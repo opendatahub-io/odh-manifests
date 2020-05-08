8abcfc769b271450a09d01df9875adef3220b3da
Chad Roberts <croberts@redhat.com>
2020-05-08 15:08:22 -0400

    Fixing up manifests in kfctl_openshift.yaml to point at PR sha

--- tests/scripts/install.sh
+++ tests/scripts/install.sh
@@ -4,6 +4,8 @@ echo "Installing kfDef from test directory"
 
 set -x
 pushd /kfdef
+echo "Setting manifests in kfctl_openshift to use sha: $PULL_PULL_SHA"
+sed -i "s#uri: https://github.com/opendatahub-io/odh-manifests/tarball/master#uri: https://github.com/opendatahub-io/odh-manifests/tarball/$PULL_PULL_SHA#" ./kfctl_openshift.yaml
 kfctl build -V -f ./kfctl_openshift.yaml
 kfctl apply -V -f ./kfctl_openshift.yaml
 set +x
