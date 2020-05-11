353fb6698232430dcdabac3f0cd5114afee72f16
Chad Roberts <croberts@redhat.com>
2020-05-11 09:52:49 -0400

    applying PR patch to master to get proper manifests for test run

--- tests/scripts/install.sh
+++ tests/scripts/install.sh
@@ -3,9 +3,15 @@
 echo "Installing kfDef from test directory"
 
 set -x
+## Grabbing and applying the patch in the PR we are testing
+pushd /src/odh-manifests
+curl -O https://github.com/${REPO_OWNER}/${REPO_NAME}/pull/${PULL_NUMBER}.patch
+git apply ${PULL_NUMBER}.patch
+popd
+## Point kfctl_openshift.yaml to the manifests in the PR
 pushd /kfdef
 echo "Setting manifests in kfctl_openshift to use sha: $PULL_PULL_SHA"
-sed -i "s#uri: https://github.com/opendatahub-io/odh-manifests/tarball/master#uri: https://github.com/opendatahub-io/odh-manifests/tarball/$PULL_PULL_SHA#" ./kfctl_openshift.yaml
+sed -i "s#uri: https://github.com/opendatahub-io/odh-manifests/tarball/master#uri: /src/odh-manifests#" ./kfctl_openshift.yaml
 kfctl build -V -f ./kfctl_openshift.yaml
 kfctl apply -V -f ./kfctl_openshift.yaml
 set +x
