1c0673111aafff83be020d13a0b6135f9bb65f72
Chad Roberts <croberts@redhat.com>
2020-06-02 11:35:11 -0400

    Working around oc status issue and getting tarball from PR to test with

--- tests/scripts/install.sh
+++ tests/scripts/install.sh
@@ -4,15 +4,10 @@ echo "Installing kfDef from test directory"
 
 set -x
 ## Install the opendatahub-operator
-## Debugging pause
-echo "Debugging pause for 3 hours"
-sleep 180m
 pushd /peak
-echo "Here is the operatorsetup file:"
-cat /peak/operatorsetup
 ./setup.sh -o /peak/operatorsetup 2>&1
-echo "Pausing 1 minute to allow operator to start"
-sleep 1m
+echo "Pausing 20 seconds to allow operator to start"
+sleep 20s
 popd
 ## Grabbing and applying the patch in the PR we are testing
 pushd /src/odh-manifests
@@ -23,8 +18,8 @@ git apply ${PULL_NUMBER}.patch
 popd
 ## Point kfctl_openshift.yaml to the manifests in the PR
 pushd /kfdef
-echo "Setting manifests in kfctl_openshift to use sha: $PULL_PULL_SHA"
-sed -i "s#uri: https://github.com/opendatahub-io/odh-manifests/tarball/master#uri: /src/odh-manifests#" ./kfctl_openshift.yaml
+echo "Setting manifests in kfctl_openshift to use pull number: $PULL_NUMBER"
+sed -i "s#uri: https://github.com/opendatahub-io/odh-manifests/tarball/master#uri: https://api.github.com/repos/opendatahub-io/odh-manifests/tarball/pull/${PULL_NUMBER}/head#" ./kfctl_openshift.yaml
 echo "Creating the following KfDef"
 cat ./kfctl_openshift.yaml
 oc create -f ./kfctl_openshift.yaml
--- tests/scripts/installandtest.sh
+++ tests/scripts/installandtest.sh
@@ -6,9 +6,14 @@ mkdir -p ~/.kube
 cp /var/run/secrets/ci.openshift.io/multi-stage/kubeconfig ~/.kube/config
 chmod 755 ~/.kube/config
 export KUBECONFIG=~/.kube/config
+# This is needed to avoid `oc status` failing inside openshift-ci
+oc new-project opendatahub
 /peak/install.sh
-/peak/run.sh basictests
+/peak/run.sh odh-manifests
 if [ "$?" -ne 0 ]; then
     echo "The tests failed"
     exit 1
 fi
+## Debugging pause
+echo "Debugging pause for 3 hours"
+sleep 180m
