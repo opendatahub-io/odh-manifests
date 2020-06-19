fecb08b1af160d6f64b92d8c78a459b3b470e916
Chad Roberts <croberts@redhat.com>
2020-06-19 12:46:46 -0400

    Adding functionality tests for ai library. (#118)
    * Adding functionality tests for ai library.
    
    * Update to include the functionalitytests in the test image
    
    * Including ai_library_cr.yaml in test container image
    
    * Using try_until_text for the seldondeployment readiness and ui curl.

--- tests/scripts/installandtest.sh
+++ tests/scripts/installandtest.sh
@@ -9,13 +9,14 @@ export KUBECONFIG=~/.kube/config
 # This is needed to avoid `oc status` failing inside openshift-ci
 oc new-project opendatahub
 /peak/install.sh
-/peak/run.sh odh-manifests
+/peak/run.sh basictests && /peak/run.sh functionalitytests
 if [ "$?" -ne 0 ]; then
+    echo "The tests failed"
     echo "Here's a dump of the pods:"
     oc get pods -o json -n opendatahub 
-    echo "The tests failed"
     exit 1
 fi
+
 ## Debugging pause...uncomment below to be able to poke around the test pod post-test
 # echo "Debugging pause for 3 hours"
 # sleep 180m
