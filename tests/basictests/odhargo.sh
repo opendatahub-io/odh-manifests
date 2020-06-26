389a1cf85371446a253cdf8c752d1be4b11d6e46
Juana Nakfour <jnakfour@redhat.com>
2020-06-26 12:05:08 -0500

    Adding an example workflow test for argo (#124)
    Co-authored-by: Juana Nakfour <nakfour>

--- tests/basictests/odhargo.sh
+++ tests/basictests/odhargo.sh
@@ -5,8 +5,17 @@ source $TEST_DIR/common
 MY_DIR=$(readlink -f `dirname "${BASH_SOURCE[0]}"`)
 
 source ${MY_DIR}/../util
+HELLOWORD_CR="${MY_DIR}/../resources/argo-hello-world.yaml"
+
 
 os::test::junit::declare_suite_start "$MY_SCRIPT"
+
+function run_helloworld() {
+    os::cmd::expect_success "oc create -f ${HELLOWORD_CR}"
+    header "The hello world example should complete a hello-world workflow with success"
+    os::cmd::try_until_text 'oc get workflow -o jsonpath="{$.items[*].status.phase}"' "Succeeded"
+}
+
 
 function test_odhargo() {
     header "Testing ODH Argo installation"
@@ -18,6 +27,7 @@ function test_odhargo() {
     os::cmd::try_until_text "oc get pods -l app=workflow-controller --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}'" "workflow-controller" $odhdefaulttimeout $odhdefaultinterval
     runningpods=($(oc get pods -l app=workflow-controller --field-selector="status.phase=Running" -o jsonpath="{$.items[*].metadata.name}"))
     os::cmd::expect_success_and_text "echo ${#runningpods[@]}" "1"
+    run_helloworld
 }
 
 test_odhargo
