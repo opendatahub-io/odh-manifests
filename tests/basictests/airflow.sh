8574c39e59c4d5956739509c0d1f62c3485a171e
Vedant Mahabaleshwarkar <vmahabal@redhat.com>
2020-06-30 11:18:29 -0400

    Added functionality tests for airflow

--- tests/basictests/airflow.sh
+++ tests/basictests/airflow.sh
@@ -6,7 +6,37 @@ MY_DIR=$(readlink -f `dirname "${BASH_SOURCE[0]}"`)
 
 source ${MY_DIR}/../util
 
+AIRFLOWBASE_CR="${MY_DIR}/../resources/airflowbase.yaml"
+AIRFLOWCLUSTER_CR="${MY_DIR}/../resources/airflowcluster.yaml"
+
 os::test::junit::declare_suite_start "$MY_SCRIPT"
+
+
+function create_airflow(){
+  header "Creating AirflowBase and AirflowCluster"
+  os::cmd::expect_success "oc create -f ${AIRFLOWBASE_CR}"
+  os::cmd::expect_success "oc create -f ${AIRFLOWCLUSTER_CR}"
+}
+
+function test_routes(){
+  header "Testing Flower and AirflowUI Routes"
+  airflowuiroute=$(oc get route pc-cluster-airflowui -o jsonpath="{$.status.ingress[0].host}")
+  flowerroute=$(oc get route pc-cluster-flower -o jsonpath="{$.status.ingress[0].host}")
+  os::cmd::try_until_text "curl -k http://$flowerroute/admin/" "Airflow - DAGs"
+  os::cmd::try_until_text "curl -k http://$flowerroute" "Flower"
+}
+
+function verify_airflow(){
+  header "Verifying Pods for Airflow"
+  os::cmd::try_until_text "oc get pods -l statefulset.kubernetes.io/pod-name=pc-base-nfs-0 --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}'" "pc-base-nfs-0"
+  os::cmd::try_until_text "oc get pods -l statefulset.kubernetes.io/pod-name=pc-base-postgres-0 --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}'" "pc-base-postgres-0"
+  os::cmd::try_until_text "oc get pods -l statefulset.kubernetes.io/pod-name=pc-cluster-airflowui-0 --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}'" "pc-cluster-airflowui-0"
+  os::cmd::try_until_text "oc get pods -l statefulset.kubernetes.io/pod-name=pc-cluster-flower-0 --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}'" "pc-cluster-flower-0"
+  os::cmd::try_until_text "oc get pods -l statefulset.kubernetes.io/pod-name=pc-cluster-redis-0 --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}'" "pc-cluster-redis-0"
+  os::cmd::try_until_text "oc get pods -l statefulset.kubernetes.io/pod-name=pc-cluster-scheduler-0 --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}'" "pc-cluster-scheduler-0"
+  os::cmd::try_until_text "oc get pods -l statefulset.kubernetes.io/pod-name=pc-cluster-worker-0 --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}'" "pc-cluster-worker-0"
+  os::cmd::try_until_text "oc get pods -l statefulset.kubernetes.io/pod-name=pc-cluster-worker-1 --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}'" "pc-cluster-worker-1"
+}
 
 function test_airflow() {
     header "Testing Airflow installation"
