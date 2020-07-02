301cca9d96270eaeafac0539e3f203c76bc2c9fa
Chad Roberts <croberts@redhat.com>
2020-07-02 09:48:21 -0400

    Do not merge.  This change turns the tests into a "dummy" that only exists so we have time to get a terminal inside the test container on the test cluster to debug parts of the installation that are problematic.

--- tests/scripts/installandtest.sh
+++ tests/scripts/installandtest.sh
@@ -9,14 +9,14 @@ export KUBECONFIG=~/.kube/config
 # This is needed to avoid `oc status` failing inside openshift-ci
 oc new-project opendatahub
 /peak/install.sh
-/peak/run.sh basictests
-if [ "$?" -ne 0 ]; then
-    echo "The tests failed"
-    echo "Here's a dump of the pods:"
-    oc get pods -o json -n opendatahub 
-    exit 1
-fi
+#/peak/run.sh basictests
+# if [ "$?" -ne 0 ]; then
+#     echo "The tests failed"
+#     echo "Here's a dump of the pods:"
+#     oc get pods -o json -n opendatahub 
+#     exit 1
+# fi
 
 ## Debugging pause...uncomment below to be able to poke around the test pod post-test
 # echo "Debugging pause for 3 hours"
-# sleep 180m
+sleep 180m
