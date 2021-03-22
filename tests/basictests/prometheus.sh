05f8f2ff211ac72b79f849ee566ec3e00182578d
Anish Asthana <anishasthana1@gmail.com>
2021-03-22 11:05:24 -0400

    Update Prometheus Object
    1. Increase number of prometheus replicas for HA
    2. Update Prometheus labels
    3. Update Prometheus Tests to account for new label
    
    Signed-off-by: Anish Asthana <anishasthana1@gmail.com>

--- tests/basictests/prometheus.sh
+++ tests/basictests/prometheus.sh
@@ -20,8 +20,8 @@ function test_prometheus() {
     os::cmd::try_until_text "oc get pods -l k8s-app=prometheus-operator --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}'" "prometheus-operator" $odhdefaulttimeout $odhdefaultinterval
     runningbuspods=($(oc get pods -l k8s-app=prometheus-operator --field-selector="status.phase=Running" -o jsonpath="{$.items[*].metadata.name}"))
     os::cmd::expect_success_and_text "echo ${#runningbuspods[@]}" "1"
-    os::cmd::try_until_text "oc get pods -l app=prometheus --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}'" "prometheus-prometheus" $odhdefaulttimeout $odhdefaultinterval
-    runningbuspods=($(oc get pods -l app=prometheus --field-selector="status.phase=Running" -o jsonpath="{$.items[*].metadata.name}"))
+    os::cmd::try_until_text "oc get pods -l app=odh-prometheus --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}'" "prometheus-prometheus" $odhdefaulttimeout $odhdefaultinterval
+    runningbuspods=($(oc get pods -l app=odh-prometheus --field-selector="status.phase=Running" -o jsonpath="{$.items[*].metadata.name}"))
     os::cmd::expect_success_and_text "echo ${#runningbuspods[@]}" "1"
     test_promportal
 }
