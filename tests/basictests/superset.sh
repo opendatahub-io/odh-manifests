11ac02a3c2a92fcc2634d143dea578f2f69348ba
Maulik Shah <maulik.shah1995@gmail.com>
2021-06-10 14:46:15 -0400

    Upgrade superset to 1.1.0 (#426)

--- tests/basictests/superset.sh
+++ tests/basictests/superset.sh
@@ -11,7 +11,7 @@ os::test::junit::declare_suite_start "$MY_SCRIPT"
 function test_superset() {
     header "Testing ODH Superset installation"
     os::cmd::expect_success "oc project ${ODHPROJECT}"
-    os::cmd::try_until_text "oc get deploymentconfig superset" "superset" $odhdefaulttimeout $odhdefaultinterval
+    os::cmd::try_until_text "oc get deployment superset" "superset" $odhdefaulttimeout $odhdefaultinterval
     os::cmd::try_until_text "oc get pods -l app=superset --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}'" "superset" $odhdefaulttimeout $odhdefaultinterval
     runningpods=($(oc get pods -l app=superset --field-selector="status.phase=Running" -o jsonpath="{$.items[*].metadata.name}"))
     os::cmd::expect_success_and_text "echo ${#runningpods[@]}" "1"
